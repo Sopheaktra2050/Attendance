74bf17b05f1c2c4825509268951423a4be4db1fd
Sopheaktra <xiidouy@gmail.com>
2020-11-16 19:05:39 +0700

    New File

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var config = {
+	 apiKey: "AIzaSyDAicXyg5ziMeIEF2QSBNqrMH16Tm633WU",
+    authDomain: "global-chat-e5840.firebaseapp.com",
+    databaseURL: "https://global-chat-e5840.firebaseio.com",
+    projectId: "global-chat-e5840",
+    storageBucket: "global-chat-e5840.appspot.com",
+    messagingSenderId: "447651600043",
+    appId: "1:447651600043:web:108cf3a60f1d38a9b909ee",
+    measurementId: "G-9YT62KTZ0T"
+};
+firebase.initializeApp(config);
